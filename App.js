ec5d082d5df34b5c905ba67a02460146a7ac6dc2
Thunder04Wolf <pa.garcia0401@gmail.com>
2024-08-04 16:40:04 -0400

    adding local storage to repo

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import RegisterVisitScreen from './screens/RegisterVisitScreen';
+import ViewVisitsScreen from './screens/ViewVisitScreen';
+import VisitDetailScreen from './screens/VisitDetailScreen';
+import AboutScreen from './screens/AboutScreen'; 
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="RegisterVisit" component={RegisterVisitScreen} />
+        <Stack.Screen name="ViewVisits" component={ViewVisitsScreen} />
+        <Stack.Screen name="VisitDetail" component={VisitDetailScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
